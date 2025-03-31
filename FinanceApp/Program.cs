ef9baa8650e06d60459f9b259d06c328d3d83027
Jakub Raus <jakub.raus@microsoft.wsei.edu.pl>
2025-04-01 00:18:59 +0200

    controller, service and chart

--- FinanceApp/Program.cs
+++ FinanceApp/Program.cs
@@ -1,4 +1,5 @@
 using FinanceApp.Data;
+using FinanceApp.Data.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<FinanceAppContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
-
+builder.Services.AddScoped<IExpensesService, ExpensesService>();
 
 var app = builder.Build();
 
