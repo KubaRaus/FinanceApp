f4282d7cb890ab8c6bd7a2d3cd40816835098ffe
Jakub Raus <jakub.raus@microsoft.wsei.edu.pl>
2025-04-08 21:31:24 +0200

    Added external Api and currency calculator

--- FinanceApp/Program.cs
+++ FinanceApp/Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddDbContext<FinanceAppContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
 builder.Services.AddScoped<IExpensesService, ExpensesService>();
 
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
